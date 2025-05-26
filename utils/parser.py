d21d22344b2f8e1a2468df1d0b457911ead82e51
qkrwhdah03 <qkrwhdah03@naver.com>
2025-05-26 07:24:55 +0000

    parser for convert images using pathlib

--- utils/parser.py
+++ utils/parser.py
@@ -1,5 +1,5 @@
 import argparse
-import os
+from pathlib import Path
 
 
 class Parser:
@@ -8,14 +8,14 @@ class Parser:
         
         self.parser.add_argument(
             "--data_dir",
-            type=str,
+            type=Path,
             required=True,
             help="Path to the source data directory"
         )
 
         self.parser.add_argument(
             "--target_dir",
-            type=str,
+            type=Path,
             required=True,
             help="Path to the target output directory"
         )
@@ -23,12 +23,14 @@ class Parser:
     def parse(self,):
         args = self.parser.parse_args()
 
-        if not os.path.exists(args.data_dir):
-             raise FileNotFoundError(f"Data directory not found: {args.data_dir}")
+        if not args.data_dir.exists():
+            raise FileNotFoundError(f"Data directory not found: {args.data_dir}")
+        if not args.data_dir.is_dir():
+            raise NotADirectoryError(f"Data directory is not a folder: {args.data_dir}")
     
-        if not os.path.exists(args.target_dir):
+        if not args.target_dir.exists():
             print(f"Target directory does not exist. Creating: {args.target_dir}")
-            os.makedirs(args.target_dir)
+            args.target_dir.mkdir(parents=True, exist_ok=True)
 
         return args
 
